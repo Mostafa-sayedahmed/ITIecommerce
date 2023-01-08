b0eca7f0ce54823660697682bc2821e846e624a7
doaaahmeds <76729277+doaaahmeds@users.noreply.github.com>
2023-01-08 22:11:24 +0200

    Add files via upload

--- signin.js
+++ signin.js
@@ -1,76 +1,42 @@
-var flg=1,flg1=0,flg2=0;
-function confemail() {
+var flg = 0, flg1 = 0, flg2 = 0;
+var archive = [];
+for (var i = 0; i < localStorage.length; i++) {
+    archive[i] = localStorage.getItem(localStorage.key(i));
+}
+function confemail2() {
     mail = document.getElementById("mail").value;
-    var httpRequestuser=new XMLHttpRequest();
-    httpRequestuser.open("GET","http://127.0.0.1:5500/users.json", true);
-    httpRequestuser.onreadystatechange = function () {
-        if (httpRequestuser.readyState == 4 && httpRequestuser.status == 200)
-         {
-                var dataRes=httpRequestuser.response;
-                var dataAfterParse=JSON.parse(dataRes);
-            for(var i=0; i<dataAfterParse.length;i++)
-            {
-                if(dataAfterParse[i].user_email==mail){
-                    flg1=1;
-                    break;
-                }   
-            }
-            if(flg1==1){
-                document.getElementById("mail").style.border = "solid 2px green";
-                
-            }else{
-                document.getElementById("mail").style.border = "solid 2px red";
-            }
+        if(archive[2]==mail){
+            flg1 = 1;
         }
-        }
-        httpRequestuser.send();
-}
-function conf() {
+        if (flg1 == 1) {
+            document.getElementById("mail").style.border = "solid 2px green";
 
-   
+        } else {
+            document.getElementById("mail").style.border = "solid 2px red";
+        }
+}
+function conf2() {
+
+
     pas = document.getElementById("pass").value;
-    var httpRequestuser=new XMLHttpRequest();
-    httpRequestuser.open("GET","http://127.0.0.1:5500/users.json", true);
-    httpRequestuser.onreadystatechange = function () {
-        if (httpRequestuser.readyState == 4 && httpRequestuser.status == 200)
-         {
-                var dataRes=httpRequestuser.response;
-                var dataAfterParse=JSON.parse(dataRes);
-            for(var i=0; i<dataAfterParse.length;i++)
-            {
-                if(dataAfterParse[i].user_password==pas){
-                   flg2=1;
-                   break;
-                }
-            }
-            if(flg2==1){
-                document.getElementById("pass").style.border = "solid 2px green";
-                
-            }else{
-                document.getElementById("pass").style.border = "solid 2px red";
-            }
+   
+        if(archive[1]==pas){
+            flg2 = 1;
         }
-        }
-        httpRequestuser.send();
+        if (flg2 == 1) {
+            document.getElementById("pass").style.border = "solid 2px green";
 
-}
-function validation(){
-    var elm=document.getElementsByTagName("input");  
-    for(var i=0;i<elm.length;i++){
-        if(elm[i].value==''){
-            flg=0;
-            break;
+        } else {
+            document.getElementById("pass").style.border = "solid 2px red";
         }
-    }  
-    if(flg==0||flg1==0||flg2==0){
-        document.getElementById("myForm").addEventListener("submit",function(e){
+}
+
+function validation1() {
+    if (flg1 == 0 || flg2 == 0) {
+        document.getElementById("myForm1").addEventListener("submit", function (e) {
             e.preventDefault();
         })
-           
-    }
-    else{
-       
-        
+
     }
 }
 
