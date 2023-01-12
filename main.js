5ad5667a818e9e917afda6b008bcc2a94c8b6196
safaa abdel naser <55063496+safaaabdelnaser@users.noreply.github.com>
2023-01-13 00:20:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,18 +11,4 @@ $(function () {
     })  
   })
 
-//search
-function search_products() {
-	let input = document.getElementById('form1').value
-	input=input.toLowerCase();
-	let x = document.getElementById('pro_name');
-	
-	for (i = 0; i < x.length; i++) {
-		if (!x[i].innerHTML.toLowerCase().includes(input)) {
-			x[i].style.display="none";
-		}
-		else {
-			x[i].style.display="list-item";				
-		}
-	}
-}
+
