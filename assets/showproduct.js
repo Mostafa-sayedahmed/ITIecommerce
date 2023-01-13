ea07c1428e572a2c21eaea5c8257a74e763c6528
safaa abdel naser <55063496+safaaabdelnaser@users.noreply.github.com>
2023-01-14 00:13:55 +0200

    Update showproduct.js

--- assets/showproduct.js
+++ assets/showproduct.js
@@ -53,3 +53,38 @@ function displayproduct(ctrl){
     }
     httpRequestall.send();
 }
+// JavaScript code
+function search_products() {
+  let cards = document.querySelectorAll('.card2');
+	let input = document.getElementById('search').value;
+	input=input.toLowerCase();
+
+	var httpRequestall = new XMLHttpRequest();
+        httpRequestall.open("GET","./products.json", true);
+        httpRequestall.onreadystatechange = function () {
+        if (httpRequestall.readyState == 4 && httpRequestall.status == 200)
+         {
+                var dataRes=httpRequestall.response;
+                var dataAfterParse=JSON.parse(dataRes);
+
+          for (var i = 0; i < cards.length; i++) {
+            if(cards[i].innerText.toLowerCase().includes(input.toLowerCase())) {
+              // ...remove the `.is-hidden` class.
+              cards[i].classList.remove("is-hidden");
+              console.log(input);
+              
+          } else {
+            // Otherwise, add the class.
+            cards[i].classList.add("is-hidden");
+          }
+
+          //   }
+        }
+    
+        }
+    }
+    httpRequestall.send();
+	
+
+}
+
